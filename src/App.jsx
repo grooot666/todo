19ca759304c468265d7ab58198055060000dfac7
24LP4133 24LP4133 <24LP4133@G304-CC0218.local>
2025-01-13 15:14:00 +0800

    wy

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React,{useState}  from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  
+  const [error,setError] = useState (false)
+  const [inputValue, setInputValue] = useState("")
+  const hanleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+  const handleAddTaskButton = () => {
+    if(inputValue.length === 0){
+      setError(true)
+    }else{
+      setError(false)
+      setTodos([...todos,{description: inputValue, status:"ACTIVE", id: uuidv4()}])
+      setInputValue("")
+    }
+   
+    
+  };
+  const HandleTaskCheckBox = (id) => {
+   const tasks = todos.map((todo) => {
+    if(todo.id === id){
+      return{...todo, status:"COMPLETED"};
+    }else{
+      return todo;
+    }
+   });
+   setTodos(tasks)
+  };
+ 
+console.log(todos);
+
+
 
   return (
     <>
     <div className='Board'>
      <div className='container'>
       <h1>To-Do list</h1>
+      {error && <div>please enter task</div>}
       <div className='addinput'>
         <div className='input'>
-        <input placeholder='Add a new task...' className='addnew'/> 
+        <input value={inputValue } onChange={hanleInputChange} placeholder='Add a new task...' className='addnew'/> 
         </div>
-        <button className='addButton'>Add</button>
+        <button onClick={handleAddTaskButton} className='addButton'>Add</button>
       </div>
+      
       <div className='buttons'>
         
          <button className='buttons1'>All</button>
@@ -24,6 +56,14 @@ function App() {
          <button className='buttons2'>Completed</button>
         
       </div>
+      {todos.map((todo, i) => {
+          return(
+            <div key={i}>
+              <input type="checkbox" 
+              onChange={() => HandleTaskCheckBox}/>
+              {todo.description}</div>
+          )
+        })}
      </div>
      <div className='notask'><p>No tasks yet. Add one above!</p></div>
      <div className='pine'>
@@ -36,3 +76,4 @@ function App() {
 }
 
 export default App
+
